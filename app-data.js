a944ad5b89a2ab9213f31da733896a7cb03cf851
Berlin Sky <pedroyoung8989@gmail.com>
2016-03-23 10:53:01 -0700

    Populate the table

---
+++ app-data.js
@@ -0,0 +1,17 @@
+var headerData = [
+	"Name", "State", "City", "Telephone", "Balance"
+];
+
+var clientData = [
+	["Ann Alexander", "CA", "Los Angelos", "509-222-2121", "50 million"],
+	["Bob Alexander", "NJ", "Jersey City", "409-222-2121", "10 million"],
+	["Cathy Alexander", "NY", "New York", "209-222-2121", "80 million"],
+	["Kevin Alexander", "GA", "Atlanda", "109-222-2121", "70 million"],
+	["Larry Alexander", "TX", "Dallas", "709-222-2121", "60 million"],
+	["Mary Alexander", "NH", "Hew Heaven", "609-222-2121", "250 million"],
+	["Nancy Alexander", "MA", "Boston", "809-222-2121", "750 million"],
+	["Oscar Alexander", "LA", "New Orleans", "449-222-2121", "850 million"],
+	["Peter Alexander", "FL", "Miami", "339-222-2121", "590 million"],
+	["Wendy Alexander", "NM", "El Paso", "229-222-2121", "450 million"],
+	["Zach Alexander", "CO", "Denvor", "789-222-2121", "50 million"]
+];
