c433ddc48bada735c7cda50165d90b73926f4a23
Berlin Sky <pedroyoung8989@gmail.com>
2016-03-24 09:21:54 -0700

    Update data

--- app-data.js
+++ app-data.js
@@ -8,7 +8,6 @@ var clientData = [
 	["Cathy Alexander", "NY", "New York", "209-222-2121", "80 million"],
 	["Kevin Alexander", "GA", "Atlanda", "109-222-2121", "70 million"],
 	["Larry Alexander", "TX", "Dallas", "709-222-2121", "60 million"],
-	["Mary Alexander", "NH", "Hew Heaven", "609-222-2121", "250 million"],
 	["Nancy Alexander", "MA", "Boston", "809-222-2121", "750 million"],
 	["Oscar Alexander", "LA", "New Orleans", "449-222-2121", "850 million"],
 	["Peter Alexander", "FL", "Miami", "339-222-2121", "590 million"],
