280d8de91424b6f13a66256f7396bc49f6b5f7cb
Berlin Sky <pedroyoung8989@gmail.com>
2016-05-05 21:12:25 -0700

    router logic

--- app.js
+++ app.js
@@ -1,8 +1,48 @@
 'use strict';
 
+ // window.ReactRouter;
+
+// const { Router, Route, Link, browserHistory } = ReactRouter;
+
+var { Router, Route, IndexRoute, Link, browserHistory } = ReactRouter
+
+// var Router = require('react-router').Router
+// var Route = require('react-router').Route
+// var Link = require('react-router').Link
+
 {
  	// 113 - ???
  }
+
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.displayName = '';
+    }
+    render() {
+        return <h1>Home</h1>;
+    }
+}
+
+class About extends React.Component {
+    constructor(props) {
+        super(props);
+        this.displayName = '';
+    }
+    render() {
+        return <h1>About</h1>;
+    }
+}
+
+class Inbox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.displayName = '';
+    }
+    render() {
+        return <h1>Inbox</h1>;
+    }
+}
 
 var App = React.createClass ({
 	render: function() {
@@ -16,8 +56,49 @@ var App = React.createClass ({
 	}
 });
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-);
+ReactDOM.render((
+ <Router history={browserHistory}>
+	<Route path="/" component={App}>
+	 <IndexRoute component={Home} />
+	</Route>
+</Router>
+), document.getElementById('app'))
+
+// const routes = {
+//   path: "\/",
+//   component: Home,
+//   indexRoute: { component: App },
+//   childRoutes: [
+//     { path: 'about', component: About },
+//     { path: 'inbox', component: Inbox },
+//   ]
+// };
+
+
+// ReactDOM.render(<App />, document.getElementById('app'));
+
+// ReactDOM.render(
+// 	(<Router history={browserHistory}>
+// 		<Route path="/" component={Home}>
+// 		 <IndexRoute component={Inbox} />
+// 		</Route>
+// 	</Router>), document.getElementById('app'));
+
+
+// var routes = {
+//   path: '\/',
+//   component: App
+// }
+
+// ReactDOM.render(<Router routes={routes} />, document.getElementById('app'));
+
+// ReactDOM.render(
+// 	(
+// 		<Router history={browserHistory}>
+// 			<Route path="/" component={App}>
+// 			</Route>
+// 		</Router>
+// 	),
+//   document.getElementById('app')
+// );
 
